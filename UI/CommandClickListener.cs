e03c1724ec79d3ab30ca5cbfaf69074f70b37358
Lukas Kurz <lukas@shortdev.de>
2025-08-11 11:59:10 +0200

    Add command click listener

---
+++ UI/CommandClickListener.cs
@@ -0,0 +1,17 @@
+﻿using Android.Views;
+using System.Windows.Input;
+
+namespace ShortDev.Android.UI;
+
+public sealed class CommandClickListener(ICommand command) : Java.Lang.Object, View.IOnClickListener
+{
+    public ICommand Command { get; } = command;
+
+    public void OnClick(View? v)
+    {
+        if (!Command.CanExecute(parameter: null))
+            return;
+
+        Command.Execute(parameter: null);
+    }
+}
